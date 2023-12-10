a094ccee02b603c9bbfb402ff8f87d26b1bb7260
devneyyputri <148979977+devneyyputri@users.noreply.github.com>
2023-12-10 09:17:42 +0700

    Update theme.js

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,14 +1,14 @@
 // theme.js
-export default {
-    colors: {
-      transparent: "transparent",
-      black: "#000",
-      white: "#fff",
-      pink: {
-        50: "#FFF5F7",
-        // ...
-        900: "#FEFCBF",
-      },
-      // ...
-    },
-  };
+// export default {
+//     colors: {
+//       transparent: "transparent",
+//       black: "#000",
+//       white: "#fff",
+//       pink: {
+//         50: "#FFF5F7",
+//         // ...
+//         900: "#FEFCBF",
+//       },
+//       // ...
+//     },
+//   };
