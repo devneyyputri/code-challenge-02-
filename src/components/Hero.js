6fc43e9b6382df8346564504587eb5541705e9b4
devneyyputri <devneyyputri@gmail.com>
2023-12-10 12:24:55 +0700

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,15 @@
-import { Box, Center, Heading, Container, Flex  } from "@chakra-ui/react"
+import { Box, Center, Heading, Container, Flex } from "@chakra-ui/react"
 import Image from "next/image";
 
 export default function Hero() {
     return (
       <Center>
+       <Flex>
       <Box height={"200px"} bg={"beige"}>
         <Container>
          <Heading className="font-mono font-large" fontSize='4vw' color='brown' as={"h1"}> Coco&apos;s Cakery </Heading>
         
-         <Flex>
+        <Flex>
          <Image 
          src="/coco's maskot.png"
          width={100}
@@ -17,10 +18,11 @@ export default function Hero() {
          />
          </Flex>
         
-             <Heading className="font-mono font-small" color='#C05621' as={"h4"} as='cite'> the best baked goods you&apos;ll ever taste! </Heading>
+             <p className="font-mono font-small" color='#C05621' as={"h4"} > the best baked goods you&apos;ll ever taste! </p>
             
           </Container>
         </Box>
+        </Flex>
         </Center> 
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Box, Flex, Container, Center } from "@chakra-ui/react"
+import Image from "next/image";
 
 export default function NavBar() {
     return (
