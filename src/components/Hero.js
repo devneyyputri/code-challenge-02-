39116a197bcdab0e5b19a94f4ed88cba1f26183f
devneyyputri <devneyyputri@gmail.com>
2023-12-10 11:43:32 +0700

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,11 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
+      <Center>
       <Box height={"200px"} bg={"beige"}>
+        <Container>
+         <Heading className="font-mono font-large" fontSize='4vw' color='brown' as={"h1"}> Coco&apos;s Cakery </Heading>
         
-        <Center>
-          
-             <Heading className="font-mono font-large" fontSize='4vw' color='brown' as={"h1"}> Coco&apos;s Cakery </Heading>
-             <Center>
          <Flex>
          <Image 
          src="/coco's maskot.png"
@@ -17,12 +16,11 @@ export default function Hero() {
          alt="maskot coco"
          />
          </Flex>
-        </Center>
-             <Heading className="font-mono font-small" color='#C05621' as={"h4"} as='cite'> the best baked goods you&apos;ll ever taste! </Heading>
-             
-          </Container>
-        </Center>
         
-      </Box>
+             <Heading className="font-mono font-small" color='#C05621' as={"h4"} as='cite'> the best baked goods you&apos;ll ever taste! </Heading>
+            
+          </Container>
+        </Box>
+        </Center> 
     );
   }
